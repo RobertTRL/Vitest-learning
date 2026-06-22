6dba8b153a02bdb0a6cd9d00573de61354f66577
Robert Toroitich <robertojnr2802@gmail.com>
2026-06-22 16:24:41 +0300

    Added a parametized test using test.for

---
+++ __tests__/parametized.test.js
@@ -0,0 +1,10 @@
+import { expect } from "chai"
+import { expected, test } from "vitest"
+
+test.for([
+    [1, 1, 2],
+    [2, 3, 5],
+    [10, 0, 10],
+])("The sum of %i and %i is %i", ([a, b, sum]) => {
+    expect(a + b).toBe(sum)
+})
