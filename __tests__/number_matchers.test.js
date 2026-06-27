41560acbe8d3466314e84076b701b73d05b1deba
Robert Toroitich <robertojnr2802@gmail.com>
2026-06-27 10:59:12 +0300

    Added example tests in number_matchers test file

--- __tests__/number_matchers.test.js
+++ __tests__/number_matchers.test.js
@@ -8,3 +8,24 @@ import { describe, test, expect } from "vitest"
 // 5) .toBe / .toEqual -> checks for equal
 // 6) .toBeCloseTo -> compares equality within a small rounding rounding errors
 
+describe("Number Matchers", () => {
+  test('number comparisons', () => {
+    const value = 2 + 2
+
+    expect(value).toBeGreaterThan(3)
+    expect(value).toBeGreaterThanOrEqual(3.5)
+    expect(value).toBeLessThan(5)
+    expect(value).toBeLessThanOrEqual(4.5)
+    expect(value).toBe(4)
+    expect(value).toEqual(4)
+})
+
+  test('adding floating point numbers', () => {
+  const value = 0.1 + 0.2
+
+  expect(value).not.toBe(0.3)
+  expect(value).toBeCloseTo(0.3)
+  
+})
+
+})
