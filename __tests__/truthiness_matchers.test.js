61af04814549b083fc68ac5483bce8ed8bcc5a97
Robert Toroitich <robertojnr2802@gmail.com>
2026-06-27 10:36:16 +0300

    Added explanatory comments in truthiness_matchers test file

---
+++ __tests__/truthiness_matchers.test.js
@@ -0,0 +1,12 @@
+
+// Truthiness Matchers
+// 1) .toBeNull -> matches ONLY null
+// 2) .toBeUndefined -> matches ONLY undefined
+// 3) .toBeDefined -> matches anything that isn't undefined, opposite of .toBeUndefined
+// 4) .toBeTruthy -> matches anything that an if statement would treat as true / truthy values
+// 5) .toBeFalsy -> matches anything that an if statement would treat as false / falsy values
+// 
+// Note:
+// Falsy values - false, 0, +0, -0, 0n, "", '', `` (Any empty string), null, undefined, NaN, document.all
+// Truthy values - any value that is not a falsy value
+// null and undefined are nullish values
