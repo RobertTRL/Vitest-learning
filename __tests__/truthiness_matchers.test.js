d4e543ac350cf0fc08fa56c77dd08f2367b6ca12
Robert Toroitich <robertojnr2802@gmail.com>
2026-06-27 10:40:11 +0300

    Added truthiness matchers tests

--- __tests__/truthiness_matchers.test.js
+++ __tests__/truthiness_matchers.test.js
@@ -1,4 +1,5 @@
 import {test, describe, expect } from "vitest"
+
 // Truthiness Matchers
 // 1) .toBeNull -> matches ONLY null
 // 2) .toBeUndefined -> matches ONLY undefined
@@ -10,3 +11,17 @@ import {test, describe, expect } from "vitest"
 // Falsy values - false, 0, +0, -0, 0n, "", '', `` (Any empty string), null, undefined, NaN, document.all
 // Truthy values - any value that is not a falsy value
 // null and undefined are nullish values
+
+describe("Truthiness Matchers", () => {
+  test('null checks', () => {
+    const n = null
+
+    expect(n).toBeNull()
+    expect(n).toBeDefined()
+    expect(n).toBeFalsy()
+    expect(n).not.toBeTruthy()
+    expect(n).not.toBeUndefined()
+
+})
+
+})
