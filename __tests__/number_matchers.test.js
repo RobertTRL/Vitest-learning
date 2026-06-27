3be2beac765b35e2d415ff36395ed4fe7808a824
Robert Toroitich <robertojnr2802@gmail.com>
2026-06-27 10:54:57 +0300

    added explanatory comments in number_matchers test file

--- __tests__/number_matchers.test.js
+++ __tests__/number_matchers.test.js
@@ -1,1 +1,10 @@
 import { describe, test, expect } from "vitest"
+
+// Number Matchers
+// 1) .toBeLessThan -> checks if the value is less than the testing value
+// 2) .toBeLessThanOrEqual -> checks if the value is less than or equal to the testing value
+// 3) .toBeGreaterThan -> checks if the value is greater than the testing value
+// 4) .toBeGreaterThanOrEqual -> checks if the value is greater than or equal to the testing value
+// 5) .toBe / .toEqual -> checks for equal
+// 6) .toBeCloseTo -> compares equality within a small rounding rounding errors
+
